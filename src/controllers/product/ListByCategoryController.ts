547e5918a9ce022c14989d6ffa06b2a154297152
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-07-14 19:48:54 -0300

    ListCategory

---
+++ src/controllers/product/ListByCategoryController.ts
@@ -0,0 +1,21 @@
+import { Request, Response } from 'express';
+import { ListByCategoryService } from '../../services/product/ListByCategoryService';
+
+class ListByCategoryController{
+    async handle(req: Request, res: Response){
+
+        const category_id = req.query.category_id as string;
+
+        const listCategoty = new ListByCategoryService();
+
+        const products = await listCategoty.execute({
+            category_id
+        });
+
+        return res.json(products);
+
+    }
+
+}
+
+export { ListByCategoryController }
