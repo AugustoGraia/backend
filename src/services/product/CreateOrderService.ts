0b8006f05387866fa90e7c4ce0bb922540edfd3e
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-07-14 20:45:59 -0300

    Create Order

---
+++ src/services/product/CreateOrderService.ts
@@ -0,0 +1,21 @@
+import prismaClient from "../../prisma";
+
+interface Order{
+    table: number;
+    name: string;
+}
+
+class CreateOrderService {
+    async execute({table, name}: Order){
+
+        const order = await prismaClient.order.create({
+            data:{
+            table: table,
+            name:name
+            }
+        })
+
+    }
+}
+
+export { CreateOrderService }
