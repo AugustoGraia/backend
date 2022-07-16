95367206cf3df8f6810044ce5583aaed131f1521
Carlos Augusto Lima <augustolimateuto@outlook.com>
2022-07-16 17:37:15 -0300

    sendorder

--- src/routes.ts
+++ src/routes.ts
@@ -16,6 +16,7 @@ import { RemoveOderController } from './controllers/order/RemoveOrderController'
 
 import { AddItemController } from './controllers/order/AddItemController';
 import { RemoveItemController } from './controllers/order/RemoveItemController';
+import { SendOrderController } from './controllers/order/SendOrderController';
 
 import { isAuthenticated } from './middlewares/isAuthenticated';
 
@@ -50,6 +51,9 @@ router.delete('/delete/order', isAuthenticated, new RemoveOderController().handl
 
 router.post('/order/add', isAuthenticated, new AddItemController().handle)
 router.delete('/order/remove', isAuthenticated, new RemoveItemController().handle)
+
+router.put('/order/send', isAuthenticated, new SendOrderController().handle)
+
 
 export { router }
 
